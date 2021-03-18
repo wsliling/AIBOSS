4ede11cc995aba9a81d79650c4ac7f4fca71cf94
wsliling <907247899@qq.com>
2021-03-18 17:57:46 +0800

    00

--- pages/tabbar/mine/mine.vue
+++ pages/tabbar/mine/mine.vue
@@ -57,27 +57,27 @@
 				<template v-slot:mainBox>
 					<view class="box-item" @click="toMoneyManagement">
 						<view class="box-item-left">
-							<image src="../../../static/mine/money_icon.png" class="image" mode=""></image>
-							<view class="menuTitle">我的钱包</view>
+							<image src="../../../static/mine/money_icon.png" mode="aspectFill"></image>
+							<view class="left-text">我的钱包</view>
 						</view>
 					</view>
 					<view class="box-item" @click="toapiLetter">
 						<view class="box-item-left">
-							<image src="../../../static/mine/API_icon.png" class="image" mode=""></image>
-							<view class="menuTitle">API授权</view>
+							<image src="../../../static/mine/API_icon.png" mode="aspectFill"></image>
+							<view class="left-text">API授权</view>
 						</view>
 					</view>
 					<view class="box-item" @click="toMyProfit">
 						<view class="box-item-left">
-							<image src="../../../static/mine/sy_icon.png" class="image" mode=""></image>
-							<view class="menuTitle">我的收益</view>
+							<image src="../../../static/mine/sy_icon.png" mode="aspectFill"></image>
+							<view class="left-text">我的收益</view>
 						</view>
 					</view>
 					<view class="box-item" data-index="team" @click="toMyTeam">
 						<view class="box-item-left">
 							<!-- <view class="iconfont icon-wodetuandui left-icon">
 							</view> -->
-							<image src="/static/mine/tuandui.png" mode=""></image>
+							<image src="/static/mine/tuandui.png" mode="aspectFill"></image>
 							<view class="left-text">我的团队</view>
 						</view>
 						<!-- <view class="box-item-right iconfont icon-dayuhao"></view> -->
@@ -86,7 +86,7 @@
 						<view class="box-item-left">
 							<!-- <view class="iconfont icon-yaoqinghaoyou left-icon fz-small">
 							</view> -->
-							<image src="/static/mine/haoyou.png" mode=""></image>
+							<image src="/static/mine/haoyou.png" mode="aspectFill"></image>
 							<view class="left-text">邀请好友</view>
 						</view>
 						<!-- <view class="box-item-right iconfont icon-dayuhao"></view> -->
@@ -95,7 +95,7 @@
 						<view class="box-item-left">
 							<!-- <view class="iconfont icon-jiaoyijilu left-icon">
 							</view> -->
-							<image src="/static/mine/jiaoyijilu.png" mode=""></image>
+							<image src="/static/mine/jiaoyijilu.png" mode="aspectFill"></image>
 							<view class="left-text">交易记录</view>
 						</view>
 						<!-- <view class="box-item-right iconfont icon-dayuhao"></view> -->
@@ -104,7 +104,7 @@
 						<view class="box-item-left">
 							<!-- <view class="iconfont icon-rizhi left-icon">
 							</view> -->
-							<image src="/static/mine/jiaoyirizhi.png" mode=""></image>
+							<image src="/static/mine/jiaoyirizhi.png" mode="aspectFill"></image>
 							<view class="left-text">交易日志</view>
 						</view>
 						<!-- <view class="box-item-right iconfont icon-dayuhao"></view> -->
@@ -113,7 +113,7 @@
 						<view class="box-item-left">
 							<!-- <view class="iconfont icon-kefu left-icon">
 							</view> -->
-							<image src="/static/mine/kefu.png" mode=""></image>
+							<image src="/static/mine/kefu.png" mode="aspectFill"></image>
 							<view class="left-text">专属客服</view>
 						</view>
 						<!-- <view class="box-item-right iconfont icon-dayuhao"></view> -->
@@ -122,7 +122,7 @@
 						<view class="box-item-left">
 							<!-- <view class="iconfont icon-kefu left-icon">
 							</view> -->
-							<image src="/static/mine/shezhi.png" mode=""></image>
+							<image src="/static/mine/shezhi.png" mode="aspectFill"></image>
 							<view class="left-text">设置</view>
 						</view>
 						<!-- <view class="box-item-right iconfont icon-dayuhao"></view> -->
